300ec9173448868ae526ad5bc88b88cdcc110493
pengyy <1348557859@qq.com>
2019-10-04 01:56:44 +0800

    myspringboot

---
+++ src/main/java/com/yy/controller/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.yy.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public String hello(){
+        return "hello world";
+    }
+	
+	@RequestMapping("/test")
+	public Map<String,String> testMap(){
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("name", "彭云云");
+		map.put("sex", "male");
+		System.out.println("test for git");
+        System.out.println("github updated file");
+        System.out.println("github to local");
+		return map;
+	}
+}
