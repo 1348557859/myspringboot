c43f1c21fffc7415db976cfe2a90bc22451bf25e
pengyy <1348557859@qq.com>
2019-10-04 11:10:09 +0800

    test sprongboot start

--- src/main/java/com/yy/HelloWorldApplication.java
+++ src/main/java/com/yy/HelloWorldApplication.java
@@ -8,6 +8,7 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("springboot start...");
 		SpringApplication.run(HelloWorldApplication.class,args);
 	}
 
